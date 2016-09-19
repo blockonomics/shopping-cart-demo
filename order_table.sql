352e1c7f1897e48fa90ee1bb961f91a5928b33e1
Srinivas <cnu.1439@gmail.com>
2016-09-19 17:49:25 +0530

    Updating orders table schema.

--- order_table.sql
+++ order_table.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `order_table` (
   `status` int(8) NOT NULL,
   `cart` text NOT NULL,
   `value` double(10,2) NOT NULL,
+  `bits_payed` int(8) NOT NULL,
   PRIMARY KEY (`order_id`),
   UNIQUE KEY `order_table` (`addr`)
   )
