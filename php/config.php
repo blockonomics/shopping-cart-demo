2bd4b4448fc58e46be024a27b93992e32420506a
Ubuntu <ubuntu@ip-172-31-10-99.us-west-2.compute.internal>
2017-10-31 04:06:06 +0000

    Adding match_callback parameter

--- php/config.php
+++ php/config.php
@@ -7,7 +7,7 @@ $CALLBACK_SECRET = 'Mabcdas122olkdd';
 
 /*Blockonomics Configurations*/
 $BASE_URL = 'https://www.blockonomics.co';
-$NEW_ADDRESS_URL = $BASE_URL.'/api/new_address';
+$NEW_ADDRESS_URL = $BASE_URL.'/api/new_address?match_callback='.$CALLBACK_SECRET;
 $PRICE_URL = $BASE_URL.'/api/price?currency=USD';
 $API_KEY = 'RRtRe4UVBLKHuk39EaWpS94WWN3MmA4QCHRm9EDpdYs';
 ?>
