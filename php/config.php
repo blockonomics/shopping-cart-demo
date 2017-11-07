76b9269cdcb6008afbe659d85b128125cc157d19
Alessandro Vecchi <aleangel1212@gmail.com>
2017-11-06 19:43:23 -0500

    Changing bootstrap for bulma

--- php/config.php
+++ php/config.php
@@ -1,5 +1,5 @@
 <?php
-$DB_HOST = '127.0.0.1';
+$DB_HOST = 'localhost';
 $DB_USER = 'root';
 $DB_PASS = 'root';
 $DB_NAME = 'shopping_cart_demo';
@@ -9,5 +9,5 @@ $CALLBACK_SECRET = 'Mabcdas122olkdd';
 $BASE_URL = 'https://www.blockonomics.co';
 $NEW_ADDRESS_URL = $BASE_URL.'/api/new_address?match_callback='.$CALLBACK_SECRET;
 $PRICE_URL = $BASE_URL.'/api/price?currency=USD';
-$API_KEY = 'RRtRe4UVBLKHuk39EaWpS94WWN3MmA4QCHRm9EDpdYs';
+$API_KEY = 'NUdcVWq0Juz29qnJH9hebgdvEY0qiSIpWZo5nCWXask';
 ?>
